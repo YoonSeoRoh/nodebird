27e70da7aecc05e0d754fc99a4f752d8bd0561e4
YoonSeoRoh <yoonseo1997@naver.com>
2022-06-15 18:17:08 +0900

    eleventh commit

---
+++ prepare/back/models/post.js
@@ -0,0 +1,33 @@
+//게시글
+const DataTypes = require("sequelize");
+const { Model } = DataTypes;
+
+module.exports = class Post extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        // id가 기본적으로 들어있다.
+        content: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        // RetweetId
+      },
+      {
+        modelName: "Post",
+        tableName: "posts",
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci", // 이모티콘 저장
+        sequelize,
+      }
+    );
+  }
+  static associate(db) {
+    db.Post.belongsTo(db.User); // post.addUser, post.getUser, post.setUser -> 하나의 포스트는 유저에 속해있음
+    db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" }); // post.addHashtags
+    db.Post.hasMany(db.Comment); // post.addComments, post.getComments -> 하나의 포스트는 여러 개의 댓글을 가지고 있음
+    db.Post.hasMany(db.Image); // post.addImages, post.getImages -> 하나의 포스트는 여러 개의 이미지를 가지고 있음
+    db.Post.belongsToMany(db.User, { through: "Like", as: "Likers" }); // post.addLikers, post.removeLikers
+    db.Post.belongsTo(db.Post, { as: "Retweet" }); // post.addRetweet
+  }
+};
---
+++ prepare/back/models/user.js
@@ -0,0 +1,49 @@
+//사용자 정보
+const DataTypes = require("sequelize");
+const { Model } = DataTypes;
+
+module.exports = class User extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        // id가 기본적으로 들어있다.
+        email: {
+          type: DataTypes.STRING(30), // STRING, TEXT, BOOLEAN, INTEGER, FLOAT, DATETIME
+          allowNull: false, // 필수
+          unique: true, // 고유한 값
+        },
+        nickname: {
+          type: DataTypes.STRING(30),
+          allowNull: false, // 필수
+        },
+        password: {
+          type: DataTypes.STRING(100),
+          allowNull: false, // 필수
+        },
+      },
+      {
+        //user model에 대한 setting
+        modelName: "User",
+        tableName: "users",
+        charset: "utf8",
+        collate: "utf8_general_ci", // 한글 저장
+        sequelize,
+      }
+    );
+  }
+  static associate(db) {
+    db.User.hasMany(db.Post); //유저와 포스트의 관계 -> 한 사람이 여러개의 포스트 작성 가능 -> 1 : M
+    db.User.hasMany(db.Comment); //유저와 댓글의 관계 -> 한 사람이 여러개의 댓글 작성 가능 -> 1: M
+    db.User.belongsToMany(db.Post, { through: "Like", as: "Liked" });
+    db.User.belongsToMany(db.User, {
+      through: "Follow",
+      as: "Followers",
+      foreignKey: "FollowingId",
+    });
+    db.User.belongsToMany(db.User, {
+      through: "Follow",
+      as: "Followings",
+      foreignKey: "FollowerId",
+    });
+  }
+};
